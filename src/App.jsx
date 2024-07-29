85ef38bf7b8558384355aaf04b9822f15aa0ee0a
Adrián Da Silva <adriandasilva23@hotmail.com>
2024-07-29 16:11:59 -0300

    Routes optimized: routes array created and mapped on app routing.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { ItemListContainer } from "./pages/ItemList/ItemListContainer";
 import { ItemDetailContainer } from "./pages/ItemDetail/ItemDetailContainer";
 import { ScreenMessage } from "./pages/ScreenMessage/ScreenMessage";
 import CartContextProvider from "./context/CartContext";
+import { routes } from "./routes/routes";
 
 function App() {
   return (
@@ -11,21 +12,11 @@ function App() {
       <CartContextProvider>
         <Routes>
           <Route element={<NavBar />}>
-            <Route path="/" element={<ItemListContainer />} />
-            <Route path="/category/:category" element={<ItemListContainer />} />
-            <Route path="/item/:id" element={<ItemDetailContainer />} />
-            <Route
-              path="/cart"
-              element={
-                <ScreenMessage message="No has agregado ningún producto al carrito." />
-              }
-            />
-            <Route
-              path="*"
-              element={
-                <ScreenMessage message="Error 404, no se ha podido encontrar la página." />
-              }
-            />
+            {routes.map(({ id, path, Element, props }) => {
+              return (
+                <Route key={id} path={path} element={<Element {...props} />} />
+              );
+            })}
           </Route>
         </Routes>
       </CartContextProvider>
---
+++ src/routes/routes.js
@@ -0,0 +1,33 @@
+import { ItemDetailContainer } from "../pages/ItemDetail/ItemDetailContainer";
+import { ItemListContainer } from "../pages/ItemList/ItemListContainer";
+import { ScreenMessage } from "../pages/ScreenMessage/ScreenMessage";
+
+export const routes = [
+  {
+    id: 1,
+    path: "/",
+    Element: ItemListContainer,
+  },
+  {
+    id: 2,
+    path: "/category/:category",
+    Element: ItemListContainer,
+  },
+  {
+    id: 3,
+    path: "/item/:id",
+    Element: ItemDetailContainer,
+  },
+  {
+    id: 4,
+    path: "/cart",
+    Element: ScreenMessage,
+    props: { message: "No has agregado ningún producto al carrito." },
+  },
+  {
+    id: 5,
+    path: "*",
+    Element: ScreenMessage,
+    props: { message: "Error 404, no se ha podido encontrar la página." },
+  },
+];
